d4ce7f3e686d5d7a543f110c65d35e0e6ed7012e
bilal <bilalmuhammed192@gmail.com>
2024-08-01 21:30:30 +0530

    starting a new life in NEXT with AI

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex items-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur debitis repellat porro rem accusantium ex cum eius tenetur facere quod quos, dolor esse nihil, eaque illum doloremque. Quisquam, quibusdam iure.
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
+};
+
+export default Home;
