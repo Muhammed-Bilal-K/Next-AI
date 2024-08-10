978f83ab83b6aedb5e16e957e3e0ba3b0f877737
bilal <muhammedbilalsaleem@gmail.com>
2024-08-10 21:43:03 +0530

    successfully implemented google auth and create post router

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,31 +2,61 @@ import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
-const handler = NextAuth({  
+const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }) {
+    callbacks: {
 
-    },
+        async session({ session }) {
+            // store the user's id coming from mongodb to session
+            const sessionUser = await User.findOne({ email: session.user.email });
+            session.user.id = sessionUser._id.toString();
 
-    async signIn({ profile }) {
-        try {
-            //serverless -> lambda -> dynamodb
-            await connectToDB();
+            return session;
+        },
 
-            //check if a user already exists
+        async signIn({ profile }) {
+            try {
+                //serverless -> lambda -> dynamodb
+                await connectToDB();
 
-            //if not, create a new user
+                //check if a user already exists
+                const userExists = await User.findOne({ email: profile.email });
+                //if not, create a new user
 
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
+                const generateUsername = (name) => {
+                    let username = name.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); 
+                    username = username.slice(0, 20); 
+                
+                    if (username.length < 3) {
+                        username = username.padEnd(3, '0'); 
+                    }
+                
+                    return username;
+                };
+
+
+                if (!userExists) {
+                    const newUsername = generateUsername(profile.name);
+
+                    await User.create({
+                        email: profile.email,
+                        username: newUsername,
+                        image: profile.picture,
+                    });
+                }
+
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
         }
     }
 })
--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptcen",
+  title: "PromptGen",
   description: "Discover & Share AI Prompts",
 };
 
